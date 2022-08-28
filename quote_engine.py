39143bacea1b93fd2b174368a0302e3827085d6a
Nick Heyer <nick@heyer.app>
2022-08-28 16:18:29 -0700

    Test Commit

---
+++ quote_engine.py
@@ -0,0 +1,31 @@
+import subsceneAPI
+import sys
+import os
+
+def get_root():
+    return os.path.dirname(__file__)
+
+
+def get_title():
+    title = None
+    if len(sys.argv) == 1:
+        title = input("What is the title you would like a random quote from?")
+    else:
+        title = sys.argv[1]
+    
+    
+
+
+
+
+
+def main():
+    
+    
+    
+    
+    return
+
+
+if __name__ == "__main__":
+    main()
